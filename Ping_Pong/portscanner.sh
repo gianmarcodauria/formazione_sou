8dc5844f706d02a23f8caac77a26b0efa3740f68
Gianmarco D'Auria <gianmarco.dauria@MBPdiGianmarco.localdomain>
2025-10-30 12:52:57 +0100

    aggiornato portscanner.sh

--- Ping_Pong/portscanner.sh
+++ Ping_Pong/portscanner.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if (( $# < 2 )); then
+        echo "Error: not enough arguments." >&2
+        exit 1
+fi
+
 host="$1"
 range="$2"
 
@@ -9,13 +14,21 @@ end="${range#*-}"
 door=()
 i=0
 
+if (( start < 1 || start > 65535 || end < 1 || end > 65535 || start > end )); then
+  echo "Error: invalid range. It should be 1-65535." >&2
+  exit 1
+fi
+
 for ((i=start; i<=end; i++)); do
-    if nc -n -w 1; then
+    if nc -n -w 1 "$host" "$i" </dev/null >/dev/null 2>&1; then
         door+=("$i")
-        echo ("$i is open")
+        echo "$i is open"
     fi
 done
 
 if (( ${#door[@]} )); then
     echo "these doors are open ${doors[@]}"
+else
+    echo "No open ports"
 fi
+
